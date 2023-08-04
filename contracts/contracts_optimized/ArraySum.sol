0f6ae8399f36e217e5bbbd791fd69aa7c968fa29
Manujaya <73481420+Iam-SilentEyes@users.noreply.github.com>
2023-08-04 23:23:02 +0530

    Optimized ArraySum

---
+++ contracts/contracts_optimized/ArraySum.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.15;
+
+contract OptimizedArraySum{
+    // Do not modify this
+    uint256[] array;
+    
+
+    // Do not modify this
+    function setArray(uint256[] memory _array) external {
+        require(_array.length <= 10, 'too long');
+        array = _array;
+    }
+
+    function getArraySum() external view returns (uint256 sum) {    
+        for (uint256 i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+
+        return sum;
+       
+    }
+
+}

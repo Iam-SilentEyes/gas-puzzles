c0b18532b5e2dcbcfb6d15c3bef04caf66e38913
Manujaya <73481420+Iam-SilentEyes@users.noreply.github.com>
2023-08-28 13:59:56 +0530

    gg

---
+++ contracts/contracts_optimized/Security101.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.15;
+
+contract OptimizedSecurity101 {
+    mapping(address => uint256) balances;
+
+    function deposit() external payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw(uint256 amount) external {
+        require(balances[msg.sender] >= amount, 'insufficient funds');
+         unchecked {
+            balances[msg.sender] -= amount;
+        }
+        (bool ok, ) = msg.sender.call{value: amount}('');
+        require(ok, 'transfer failed');
+       
+    }
+}
